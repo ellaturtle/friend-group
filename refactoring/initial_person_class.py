943bbf2a8b4828ba21c911cfbc628b2812c78fa5
Ella Turtle <ucaptur@ucl.ac.uk>
2025-11-30 15:44:34 +0000

    Adding a person class

--- refactoring/initial_person_class.py
+++ refactoring/initial_person_class.py
@@ -16,25 +16,34 @@ class Person:
 
     def forget(self, person):
         """Removes any connections to a person"""
-        pass
-
+        if person not in self.connections:
+            raise ValueError(f"There are no connections to {person.name}")
+        self.connections.pop(person)
 
 def average_age(group):
     """Compute the average age of the group's members."""
     all_ages = [person.age for person in group]
     return sum(all_ages) / len(group)
 
-
 if __name__ == "__main__":
     # ...then create the group members one by one...
     jill = Person("Jill", 26, "biologist")
+    zalika = Person("Zalika", 28, "artist")
+    john = Person("John", 27, "writer")
+    nash = Person("Nash", 34, "chef")
 
     # ...then add the connections one by one...
     # Note: this will fail from here if the person objects aren't created
     jill.add_connection(zalika, "friend")
+    jill.add_connection(john, "partner")
+    zalika.add_connection(jill, "friend")
+    john.add_connection(jill, "partner")
+    nash.add_connection(john, "cousin")
+    nash.add_connection(zalika, "landlord")
 
     # ... then forget Nash and John's connection
     nash.forget(john)
+    john.forget(nash)
     # Then create the group
     my_group = {jill, zalika, john, nash}
 
